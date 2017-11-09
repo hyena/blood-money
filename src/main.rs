1c98a422bfd43174e6da3e4eef9191f7e7ca0e1b
hyena <hyena@users.noreply.github.com>
2017-11-09 00:51:42 -0800

    Correctly calculate mat costs

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,9 @@ struct ItemValue {
 #[derive(Debug)]
 struct CurrentRealmValues {
     last_update: u64,  // The last time we got this info, as reported by the Blizzard API.
-    auction_values: Arc<Vec<ItemValue>>,  // Should be sorted by value.
+    value_map: Arc<HashMap<u64, u64>>,
+    sargerite_item_values: Arc<Vec<ItemValue>>,
+    blood_item_values: Arc<Vec<ItemValue>>,  // Should be sorted by value.
 }
 
 /// All the data in a single row in our price list for a realm.
@@ -85,7 +87,8 @@ const RESULT_FETCH_PERIOD: u64 = 60 * 30;
 /// Given a vec of auction listings for a realm and a map of the items we care about,
 /// returns a vec of (item_id, value) sorted by decreasing value, where value is
 /// based on the 5th percentile buyout price.
-fn calculate_auction_values(listings: &Vec<AuctionListing>, items: &HashMap<u64, VendorItem>) -> Vec<ItemValue> {
+fn calculate_auction_values(listings: &Vec<AuctionListing>, items: &HashMap<u64, VendorItem>) ->
+    (HashMap<u64, u64>, Vec<ItemValue>, Vec<ItemValue>) {
     // Calculate 5th percentiles for the items we care about.
     let mut price_points: BTreeMap<u64, Vec<(u64, u64)>> = BTreeMap::new();
     for listing in listings {
@@ -100,7 +103,7 @@ fn calculate_auction_values(listings: &Vec<AuctionListing>, items: &HashMap<u64,
         price_points.iter().map(|(k, v)| {
             (*k, v.iter().fold(0, |sum, quantity_and_buyout| sum + quantity_and_buyout.0))
         }).collect();
-    let fifth_percentile_price_points: BTreeMap<u64, u64> =
+    let fifth_percentile_price_points: HashMap<u64, u64> =
         price_points.iter().map(|(item_id, ref item_listings)| {
             let fifth_percentile_quantity = total_item_quantities.get(item_id).unwrap() / 20;
             let mut running_sum: u64 = 0;
@@ -110,20 +113,42 @@ fn calculate_auction_values(listings: &Vec<AuctionListing>, items: &HashMap<u64,
             }).unwrap();
             (*item_id, fifth_percentile_listing.1)
         }).collect();
-    let mut item_values: Vec<ItemValue> = items.values().map(|item|
+    let mut item_values: Vec<ItemValue> = items.values().map(|item| {
+        let mut value = *fifth_percentile_price_points.get(&item.id).unwrap_or(&0u64);
+        value = match &item.mats {  // Subtract the costs of mats if any.
+            &Some(ref mats_list) => {
+                // Sum up the prices.
+                let mat_cost = mats_list.iter().map(
+                     |&ref x| x.quantity * fifth_percentile_price_points.get(&x.id).unwrap_or(&0u64)
+                ).sum();
+                if value >= mat_cost {
+                    value - mat_cost
+                } else {
+                    0
+                }
+            },
+            &None => value
+        };
         ItemValue {
             id: item.id,
             value: match item.quantity {
                 // A bit of a hack to avoid propagating floating point madness through the codebase:
                 // Some items are 10 primal sargerite for 1 gem. All the others are whole numbers. So
                 // we special case the former case as integer division.
-                0.1 => fifth_percentile_price_points.get(&item.id).unwrap_or(&0u64) / 10,
-                _ => fifth_percentile_price_points.get(&item.id).unwrap_or(&0u64) * item.quantity as u64,
+                0.1 => value / 10,
+                _ => value * item.quantity as u64,
             }
         }
-    ).collect();
+    }).collect();
     item_values.sort_by_key(|item_value| !item_value.value);
-    item_values
+
+    // NOTE: drain_filter() is a nightly only experimental API call that might break.
+    let blood_item_values = item_values.drain_filter(|x| items.get(&x.id).unwrap().vendor_type.eq("blood"))
+        .collect::<Vec<_>>();
+    let sargerite_item_values = item_values.drain_filter(|x| items.get(&x.id).unwrap().vendor_type.eq("sargerite"))
+        .collect::<Vec<_>>();
+
+    (fifth_percentile_price_points, blood_item_values, sargerite_item_values)
 }
 
 /// Given a battle net Region, return the first path of the app's URL.
@@ -165,7 +190,9 @@ fn main() {
     let price_map: Arc<BTreeMap<String, RwLock<CurrentRealmValues>>> =
         Arc::new(realms.iter().map(|realm| (realm.slug.clone(), RwLock::new(CurrentRealmValues {
             last_update: 0,
-            auction_values: Arc::new(item_id_map.keys().map(|id| ItemValue{id: *id, value: 0u64}).collect()),
+            value_map: Arc::new(HashMap::new()),
+            sargerite_item_values: Arc::new(Vec::new()),
+            blood_item_values: Arc::new(Vec::new()),
         }))).collect());
 
     // Set up our web-app.
@@ -191,55 +218,81 @@ fn main() {
             if let Some(realm_prices_lock) = price_map.get(realm) {
                 let mut context = Context::new();
                 let realm_prices = realm_prices_lock.read().unwrap();
-                // Build up a list of entries.
-                // Find the most valuable item that isn't a reagent.
-                // TODO: This calculation is getting a little awkward. Do it at template time instead?
-                let highest_value = realm_prices.auction_values.iter()
-                    .find(|&x| item_id_map.get(&x.id).unwrap().vendor_type != "reagent")
-                    .unwrap().value;
-                let mut price_rows: Vec<PriceRow> = realm_prices.auction_values.iter().map(|&ItemValue{id, value}| {
-                    let item_info = item_id_map.get(&id).unwrap();
-                    let value = match &item_info.mats {
-                        &Some(ref mats_list) => {
-                            // TODO: Sorting the prices into a Vec was conventient, but now it's getting
-                            // Awkward to do things like look up a price. Refactor because we're doing a O(N^2) iterator
-                            // insite an iterator here.
 
-                            // Sum up the prices. This will panic if a reagent is not in the items list.
-                            let mat_cost = mats_list.iter().map(
-                                |&ref x| x.quantity * realm_prices.auction_values.iter().find(
-                                    |&y| x.id == y.id
-                                ).unwrap().value).sum();
-                            if value >= mat_cost {
-                                value - mat_cost
-                            } else {
-                                0
-                            }
-                        },
-                        &None => value
+                // Closure that processes the vectors of ItemValues into
+                // the HTML that we need.
+                // TODO: Things would actually be even cleaner probably if
+                // we did all this work in the background thread; it doesn't
+                // change per request. Then this router method would basically
+                // just take a read lock on the values, clone some things,
+                // and build the contexts.
+                let make_price_rows = |x: &Vec<ItemValue>| -> Vec<PriceRow> {
+                    let highest_value = match x.first() {
+                        Some(item_value) => item_value.value,
+                        None => 0
                     };
-                    let gold = value / (10_000);
-                    let silver = (value - gold * 10_000) / 100;
-                    let copper = value - gold * 10_000 - silver * 100;
-                    let value_ratio = match highest_value {
-                        0u64 => 0u64,
-                        _ => value*100/highest_value,  // Percentile!
-                    };
-                    PriceRow {
-                        name: item_info.name.clone(),
-                        quantity: item_info.quantity,
-                        icon: item_icons.get(&id).unwrap().clone(),
-                        subtext: item_info.subtext.clone().unwrap_or(String::new()),
-                        vendor_type: item_info.vendor_type.clone(),
-                        value_ratio: value_ratio,
-                        gold: gold,
-                        silver: silver,
-                        copper: copper,
-                    }
-                }).collect();
-                // NOTE: drain_filter() is a nightly only experimental API call that might break.
-                let blood_price_rows = price_rows.drain_filter(|x| x.vendor_type.eq("blood")).collect::<Vec<_>>();
-                let sargerite_price_rows = price_rows.drain_filter(|x| x.vendor_type.eq("sargerite")).collect::<Vec<_>>();
+                    x.iter().map(|&ItemValue{id, value}| {
+                        let item_info = item_id_map.get(&id).unwrap();
+                        let gold = value / (10_000);
+                        let silver = (value - gold * 10_000) / 100;
+                        let copper = value - gold * 10_000 - silver * 100;
+                        let value_ratio = match highest_value {
+                            0u64 => 0u64,
+                            _ => value*100/highest_value,  // Percentile!
+                        };
+                        PriceRow {
+                            name: item_info.name.clone(),
+                            quantity: item_info.quantity,
+                            icon: item_icons.get(&id).unwrap().clone(),
+                            subtext: item_info.subtext.clone().unwrap_or(String::new()),
+                            vendor_type: item_info.vendor_type.clone(),
+                            value_ratio: value_ratio,
+                            gold: gold,
+                            silver: silver,
+                            copper: copper,
+                        }
+                    }).collect()
+                };
+                // let highest_value = realm_prices.auction_values.iter()
+                //     .find(|&x| item_id_map.get(&x.id).unwrap().vendor_type != "reagent")
+                //     .unwrap().value;
+                // let mut price_rows: Vec<PriceRow> = realm_prices.auction_values.iter().map(|&ItemValue{id, value}| {
+                //     let item_info = item_id_map.get(&id).unwrap();
+                //     let value = match &item_info.mats {
+                //         &Some(ref mats_list) => {
+                //             // Sum up the prices. This will panic if a reagent is not in the items list.
+                //             let mat_cost = mats_list.iter().map(
+                //                 |&ref x| x.quantity * realm_prices.value_map.get(&x.id).unwrap()
+                //             ).sum();
+                //             if value >= mat_cost {
+                //                 value - mat_cost
+                //             } else {
+                //                 0
+                //             }
+                //         },
+                //         &None => value
+                //     };
+                //     let gold = value / (10_000);
+                //     let silver = (value - gold * 10_000) / 100;
+                //     let copper = value - gold * 10_000 - silver * 100;
+                //     let value_ratio = match highest_value {
+                //         0u64 => 0u64,
+                //         _ => value*100/highest_value,  // Percentile!
+                //     };
+                //     PriceRow {
+                //         name: item_info.name.clone(),
+                //         quantity: item_info.quantity,
+                //         icon: item_icons.get(&id).unwrap().clone(),
+                //         subtext: item_info.subtext.clone().unwrap_or(String::new()),
+                //         vendor_type: item_info.vendor_type.clone(),
+                //         value_ratio: value,
+                //         gold: gold,
+                //         silver: silver,
+                //         copper: copper,
+                //     }
+                // }).collect();
+                let blood_price_rows = make_price_rows(&realm_prices.blood_item_values);
+                let sargerite_price_rows = make_price_rows(&realm_prices.sargerite_item_values);
                 context.add("realm_name", &realms.iter().find(|&realm_info| &realm_info.slug == realm).unwrap().name);
                 context.add("blood_price_rows", &blood_price_rows);
                 context.add("sargerite_price_rows", &sargerite_price_rows);
@@ -293,12 +346,19 @@ fn main() {
                             None => return,
                         }
                     }
-                    let auction_values = Arc::new(calculate_auction_values(&auction_listings, &item_id_map));
+                    let (value_map, blood_item_values, sargerite_item_values) = calculate_auction_values(&auction_listings, &item_id_map);
+                    // Make thread safe versions of the new values.
+                    let value_map = Arc::new(value_map);
+                    let blood_item_values = Arc::new(blood_item_values);
+                    let sargerite_item_values = Arc::new(sargerite_item_values);
+                    //let auction_values = Arc::new();
                     for realm in realm_list {
                         println!("Updating {}", realm);
                         let mut current_realm_values =
                             price_map.get(realm).unwrap().write().unwrap();
-                        current_realm_values.auction_values = auction_values.clone();
+                        current_realm_values.value_map = Arc::clone(&value_map);
+                        current_realm_values.blood_item_values = Arc::clone(&blood_item_values);
+                        current_realm_values.sargerite_item_values = Arc::clone(&sargerite_item_values);
                         current_realm_values.last_update = update_time;
                     }
                 })
